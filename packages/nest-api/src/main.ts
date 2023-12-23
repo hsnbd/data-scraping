2b8a9efc1ab257386ed9a8b6365b93bc255c2013
hsnbd <hasanbd666@gmail.com>
2023-12-24 00:32:45 +0600

    feat: add global exception filter

--- packages/nest-api/src/main.ts
+++ packages/nest-api/src/main.ts
@@ -1,16 +1,20 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigKey } from './core/config/app-config';
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { AllExceptionsFilter } from './core/exception-filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  const configService: ConfigService = app.get<ConfigService>(ConfigService);
+
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
-  const configService: ConfigService = app.get<ConfigService>(ConfigService);
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
   await app.listen(configService.get<number>(ConfigKey.PORT));
 
@@ -20,4 +24,5 @@ async function bootstrap() {
     )})`,
   );
 }
-bootstrap();
+
+bootstrap().catch((error) => console.log(error));
